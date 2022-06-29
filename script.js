3e1bffcba6ffbe894da4068a88bfb636484ab577
maxroy80 <maxroy80@gmail.com>
2022-06-29 17:31:35 +0300

    local update 2

--- script.js
+++ script.js
@@ -3,22 +3,12 @@ let credentials = {
     password: "qwerty"
 };
 
-// let login = document.getElementById('login');
-// let pass = document.getElementById('pass');
-// console.log("Login - " + credentials.login + " Password - " + credentials.password);
-
 function check() {
-
-    // console.log(login.value);
-    // console.log(pass.value);
-
-    // if (credentials.login == login.value && credentials.password == pass.value) {
     if (credentials.login == document.getElementById('login').value && credentials.password == document.getElementById('pass').value) {
             alert("All correct")
     }
     else {
         alert("Wrong login or password")
     };
-    // alert("function run");
 }
 
