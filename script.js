5eded6416131d04cb86ad3ac1acba2eb5c12ec5c
maxroy80 <maxroy80@gmail.com>
2022-06-30 13:56:50 +0300

    local update 3

--- script.js
+++ script.js
@@ -3,12 +3,37 @@ let credentials = {
     password: "qwerty"
 };
 
+let answerStyle = document.getElementById('check_res');
+let checkBool;
+
 function check() {
     if (credentials.login == document.getElementById('login').value && credentials.password == document.getElementById('pass').value) {
-            alert("All correct")
+        
+        answerStyle.style.backgroundColor = "#98e69d";
+        answerStyle.style.transform = "translate(-20px, 50%)";
+        answerStyle.style.visibility = "visible";
+        checkBool = true;
+        document.getElementById('check_massage').innerHTML = "All correct";
+        return false;
+        // alert("All correct");
     }
     else {
-        alert("Wrong login or password")
+        answerStyle.style.backgroundColor = "#ff5757";
+        answerStyle.style.transform = "translate(-20px, 50%)";
+        answerStyle.style.visibility = "visible";
+        checkBool = false;
+        document.getElementById('check_massage').innerHTML = "Wrong login or password";
+        return false;
+        // alert("Wrong login or password")
     };
 }
 
+function checkDone() {
+    if (checkBool === true) {
+        // location.href = 'https://www.google.ua';
+        location.reload();
+    }
+    else {
+        location.reload();
+    }
+}
